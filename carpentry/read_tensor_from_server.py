6cf9bbd1c4162af0f15c94ceb736c0169d5ea3f9
zach3141 <zach3141@gmail.com>
2017-03-24 15:34:09 -0700

    added local tensor stuff

--- carpentry/read_tensor_from_server.py
+++ carpentry/read_tensor_from_server.py
@@ -6,36 +6,39 @@ import os
 import ftplib
 import sys
 
-input_server = "zbutler@datalab-11.ics.uci.edu:/extra/zbutler0/data/gfs"  # Where we will store raw data
+input_server = "zbutler@datalab-11.ics.uci.edu:/extra/zbutler0/data/gfs/"  # Where we will store raw data
 
 
-def get_tensor_from_server(out_fi, temp_fi, tensor_type='temp', year=2013):
+def get_tensor_from_server(out_fi, temp_fi, tensor_type='temp', year=2013, local=None):
     tensor = np.zeros((181, 360, 365), dtype=float)
 
     days_arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # Days in a month
-    day = 1
+    day = 6
     month = 1
     day_of_year = 0
 
-    while month < 13:  # Get data for every day of the year
+    while month < 9:  # Get data for every day of the year
         ymd_str = "%d%.2d%.2d" % (year, month, day)
-        os.system("scp %s/%s.grb %s" % (input_server, ymd_str, temp_fi))
+        if local:
+            grib_fi = input_server.split(":")[-1] + ymd_str + ".grb"
+        else:
+            os.system("scp %s/%s.grb %s" % (input_server, ymd_str, temp_fi))
+            grib_fi = temp_fi
 
         # Now, pull the temperature data to store locally
-        grbs = pygrib.open(temp_fi)
+        grbs = pygrib.open(grib_fi)
 
-        for layer in grbs:
-            if tensor_type.startswith('temp'):
-                if layer.name == "Temperature" and layer.level == 0:
-                    my_layer = layer
-                    break
-            elif tensor_type.startswith('hum'):
-                if layer.name == "Relative humidity" and layer.typeOfLevelECMF == "entireAtmosphere":
-                    my_layer = layer
-                    break
-            else:
-                raise ValueError("Unknown tensor type")
-        tensor[:,:,day_of_year] = my_layer.values
+        if tensor_type.startswith('temp'):
+            layer = grbs[211]
+            if layer.name != "Temperature" or layer.level != 0:
+                raise ValueError("Incorrect layer on month %d day %d" % (month, day))
+        elif tensor_type.startswith('hum'):
+            layer = grbs[234]
+            if 'humidity' not in layer.name:
+                raise ValueError("Incorrect layer on month %d day %d" % (month, day))
+        else:
+            raise ValueError("Unknown tensor type")
+        tensor[:,:,day_of_year] = layer.values
 
         print "Finished day %d, month %d" % (day, month)
         if day == days_arr[month-1]:
