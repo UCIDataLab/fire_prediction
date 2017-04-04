92d03a8781b2a906f6aa66572b448b6778311b25
zach3141 <zach3141@gmail.com>
2017-04-04 16:26:17 -0700

    Changed read_tensor to read_dict

--- carpentry/read_dict_from_server.py
+++ carpentry/read_dict_from_server.py
@@ -9,13 +9,14 @@ import sys
 input_server = "zbutler@datalab-11.ics.uci.edu:/extra/zbutler0/data/gfs/"  # Where we will store raw data
 
 
-def get_tensor_from_server(out_fi, temp_fi, tensor_type='temp', year=2013, local=True):
-    tensor = np.zeros((361, 720, 365), dtype=float)
+def get_dict_from_server(out_fi, temp_fi, tensor_type='temp', year=2013, local=True):
+    res_dict = dict()
 
     days_arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # Days in a month
     day = 1
     month = 1
     day_of_year = 0
+    first_grib = 1
 
     while month < 13:  # Get data for every day of the year
         ymd_str = "%d%.2d%.2d" % (year, month, day)
@@ -28,22 +29,21 @@ def get_tensor_from_server(out_fi, temp_fi, tensor_type='temp', year=2013, local
         # Now, pull the temperature data to store locally
         grbs = pygrib.open(grib_fi)
 
-        for i,_ in enumerate(grbs):
-            pass
-        print "day %d, month %d, %d layers" % (day, month, i)
-
         if tensor_type.startswith('temp'):
-            layer = grbs[211]
-            if layer.name != "Temperature" or layer.level != 0:
-                raise ValueError("Incorrect layer on month %d day %d" % (month, day))
+            layer = grbs.select(name='Temperature',typeOfLevel='surface')[0]
         elif tensor_type.startswith('hum'):
-            layer = grbs[234]
-            if 'humidity' not in layer.name:
-                raise ValueError("Incorrect layer on month %d day %d" % (month, day))
+            layer = grbs.select(name='Relative humidity', level=0, typeOfLevel='unknown',
+                                typeOfFirstFixedSurface='200')[0]
         else:
             raise ValueError("Unknown tensor type")
-        tensor[:,:,day_of_year] = layer.values
+        res_dict[(month, day)] = layer.values
+        if first_grib:
+            lats,lons = layer.latlons()
+            res_dict['lats'] = lats
+            res_dict['lons'] = lons
+            first_grib = 0
 
+        print "Finished processing month %d/%d" % (month, day)
         if day == days_arr[month-1]:
             day = 1
             month += 1
@@ -52,8 +52,8 @@ def get_tensor_from_server(out_fi, temp_fi, tensor_type='temp', year=2013, local
         day_of_year += 1
 
     with open(out_fi,"w") as fout:
-        cPickle.dump(tensor, fout, cPickle.HIGHEST_PROTOCOL)
+        cPickle.dump(res_dict, fout, cPickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
-    get_tensor_from_server(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
+    get_dict_from_server(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
