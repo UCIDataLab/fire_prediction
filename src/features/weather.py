a1e0ef5788f601b04a99a7f908caa0dab0f8eef1
Casey <caseyagraff@gmail.com>
2017-08-31 15:14:22 -0700

    Added weather processing

---
+++ src/features/weather.py
@@ -0,0 +1,109 @@
+"""
+Code for handling global weather data.
+"""
+import bisect
+
+class WeatherCube(object):
+    def __init__(self, name, values, units, bounding_box=None, axis_labels=None, dates=None):
+        """
+        Assumes that dates is as long as values and is ordered.
+        """
+        self.name = name
+        self.values = values
+        self.units = units
+        self.bounding_box = bounding_box
+        self.axis_labels = axis_labels
+        self.dates = dates
+
+        self.attributes = {}
+        self.shape = values.shape
+
+    def add_attribute(self, name, value):
+        self.attributes[name] = value
+
+    def filter_dates(self, date_start, date_end):
+        ind_start = bisect.bisect_left(self.dates, date_start)
+        ind_end = bisect.bisect_right(self.dates, date_end)
+
+        new = WeatherCube(self.name, self.values[:,:,ind_start:ind_end], self.units, self.bounding_box, self.axis_labels, self.dates[ind_start:ind_end])
+        for k,v in self.attributes.iteritems():
+            if len(v) == len(self.dates):
+                new.add_attribute(k, v[ind_start:ind_end])
+            else:
+                new.add_attribute(k, v)
+
+        return new
+
+    def get_values_for_date(self, date):
+        """
+        Return all values with matching date.
+        """
+        ind_start = bisect.bisect_left(self.dates, date)
+        ind_end = bisect.bisect_right(self.dates, date)
+
+        return self.values[:, :, ind_start:ind_end]
+
+    def get_attribute_for_date(self, name, date):
+        """
+        Return all values with matching date.
+        """
+        # TODO support per item attributes
+        attribute = self.attributes[name]
+
+        ind_start = bisect.bisect_left(self.dates, date)
+        ind_end = bisect.bisect_right(self.dates, date)
+
+        return attribute[ind_start:ind_end]
+
+
+class WeatherRegion(object):
+    """
+    A collection of WeatherCubes with matching shape and dates.
+    """
+
+    def __init__(self, name, cubes={}):
+        self.name = name
+
+        self.cubes = {}
+        for _,cube in cubes.iteritems():
+            self.add_cube(cube)
+            
+        self.shape = None
+        self.dates = None
+
+    # TODO: Free memory for repeated shape, dates and bounding_box?
+    def add_cube(self, cube):
+        if self.shape and (cube.shape != self.shape):
+            raise ValueError('All cubes in a region must have the same shape. Cube shape %s != region shape %s' % (cube.shape, self.shape))
+
+        if self.dates and (cube.dates != self.dates):
+            raise ValueError('All cubes in a region must have the same dates' % (np.shape(cube.data), self.shape))
+
+        if self.bounding_box and (cube.bounding_box != self.bounding_box):
+            raise ValueError('All cubes in a region must have the same bounding box. Cube bb %s != region bb %s' % (str(cube.bounding_box), str(self.bounding_box)))
+
+        self.shape = cube.shape
+        self.dates = cube.dates
+        self.bounding_box = cube.bounding_box
+
+        self.cubes[cube.name] = cube
+
+    def remove_cube(self, cube_name):
+        del self.cubes[cube_name]
+
+    def __getitem__(self, key):
+        return self.cubes[key]
+
+    def filter_dates(self, date_start, date_end):
+        ind_start = bisect.bisect_left(self.dates, date_start)
+        ind_end = bisect.bisect_right(self.dates, date_end)
+
+        new_cubes = {}
+        for name,cube in self.cubes.iteritems():
+            new_cubes[name] = cube.filter_dates(date_start, date_end)
+
+        new = WeatherRegion(self.name, new_cubes)
+
+        return new
+
+
---
+++ src/features/weather_processing.py
@@ -0,0 +1,105 @@
+"""
+Processing the weather data for integration.
+"""
+import click
+import logging
+import cPickle as pickle
+import numpy as np
+from collections import deque
+
+import weather
+from base.converter import Converter
+
+class WeatherRegionProcessing(Converter):
+    """
+    Process a weather region to prepare it for integration.
+    """
+    def __init__(self):
+        super(WeatherRegionProcessing, self).__init__()
+
+    def load(self, src_path):
+        logging.info('Loading file from %s' % src_path)
+        with open(src_path, 'rb') as fin:
+            data = pickle.load(fin)
+
+        return data
+
+    def save(self, dest_path, data):
+        logging.info('Saving data frame to %s' % dest_path)
+
+        with open(dest_path, 'wb') as fout:
+            pickle.dump(data, fout, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def transform(self, data):
+        self.combine_wind_components(data)
+        self.integrate_rain(data)
+        data = self.remove_offset_measurements(data)
+
+        return data
+
+    def combine_wind_components(self, data):
+        u_wind_cube = data['u_wind_component']
+        v_wind_cube = data['v_wind_component']
+
+        wind_values = np.sqrt(u_wind_cube.values**2 + v_wind_cube.values**2)
+        wind_cube = weather.WeatherCube('wind', wind_values, u_wind_cube.units, u_wind_cube.bounding_box, u_wind_cube.axis_labels, u_wind_cube.dates)
+
+        data.remove_cube('u_wind_component')
+        data.remove_cube('v_wind_component')
+
+        data.add_cube(wind_cube)
+
+    def integrate_rain(self, weather_region):
+        rain_cube = data['total_precipitation']
+
+        integrated_rain_values = np.empty(rain_cube.shape)
+        integrated_rain_values.fill(np.nan)
+
+        for i in range(12, rain_cube.shape[2], 3):
+            val = rain_cube.values[:,:,i-10] + rain_cube.values[:,:,i-7] + rain_cube.values[:,:,i-4] + rain_cube.values[:,:,i-1]
+            integrated_rain_values[:,:,i] = val
+
+        integrated_rain_cube = weather.WeatherCube('rain', integrated_rain_values, rain_cube.units, rain_cube.bounding_box, rain_cube.axis_labels, rain_cube.dates)
+
+        data.remove_cube('total_precipitation')
+
+        data.add_cube(integrated_rain_cube)
+
+    def remove_offset_measurements(self, data):
+        new_region = weather.WeatherRegion(data.name)
+
+        for _, cube in data.cubes.iteritems():
+            new_cube = self.remove_offset_measurements_cube(cube)
+            new_region.add_cube(new_cube)
+
+        return new_region
+
+    def remove_offset_measurements_cube(self, cube):
+        if cube.shape % 3 != 0:
+            raise ValueError('Cube shape must be divisible by three (+0, +3, +6 offsets). Shape of %s is %s.' % (cube.name, str(cube.shape)))
+
+        new_values = cube.values[:,:,::3].copy()
+        new_dates = cube.dates[::3]
+
+        new_cube = weather.WeatherCube(cube.name, new_values, cube.units, cube.bounding_box, cube.axis_labels, new_dates)
+
+        return new_cube
+
+@click.command()
+@click.argument('src_path', type=click.Path(exists=True))
+@click.argument('dest_path')
+@click.option('--log', default='INFO')
+def main(src_path, dest_path, log):
+    """
+    Load fire data frame and create clusters.
+    """
+    log_fmt = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=getattr(logging, log.upper()), format=log_fmt)
+
+    logging.info('Starting weather processing')
+    WeatherRegionProcessing().convert(src_path, dest_path)
+    logging.info('Finished weather processing')
+
+
+if __name__ == '__main__':
+    main()
